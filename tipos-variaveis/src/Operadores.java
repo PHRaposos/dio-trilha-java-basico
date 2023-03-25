75e17f4fd68034dda759cf280418afe4797025bf
PHRaposos <ph.ramom2003@gmail.com>
2023-03-25 20:55:44 -0300

    Conclusão do Curso!

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,61 @@
+public class Operadores {
+  public static void main(String[] args) throws Exception {
+    
+    //classe Operadores.java
+    /*String nome = "PEDRO HENRIQUE";
+    int idade = 19;
+    double peso = 112;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date(15/07/2003);*/
+
+    //classe Operadores.java
+    /*double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4);*/
+
+    // String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+    // System.out.println(nomeCompleto);
+        
+
+    // String concatenacao = "?";
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1+1+1+"1";
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1+"1"+1+1;
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1+"1"+1+"1";
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = "1"+1+1+1;
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = "1"+(1+1+1);
+
+    // System.out.println(concatenacao);
+        
+    // int numero = 5;
+    
+    // System.out.println(- numero);
+
+    // int numero = 5;
+
+    // numero = numero + 2;
+    // numero = numero + 1;
+
+    // System.out.println(numero);
+
+  }
+}
